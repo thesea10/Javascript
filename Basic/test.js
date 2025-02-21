c92724e434fe81f368161f136f8d6e17ea6afd6b
thesea10 <sagarbhattarai138@gmail.com>
2025-02-21 15:16:26 +0000

    new

--- Basic/test.js
+++ Basic/test.js
@@ -1,1 +1,15 @@
+"use strict"; //treat all JS code as newer version
+
 console.log("Hello")
+const accountId = 123456
+let accountEmail = "sagarbhattarai@gmail.com"
+var accountPassword = "123456"
+accountCity = "Kouvola"
+// AccountId = 11
+console.log(accountId);
+
+
+
+
+
+ 
